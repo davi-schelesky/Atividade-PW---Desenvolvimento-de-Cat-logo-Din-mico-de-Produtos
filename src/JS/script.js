5d857e60571335615fa0dc5ac398d310ba0b2090
davi-schelesky <davischelesky2011@gmail.com>
2026-09-22 12:48:34 -0300

    First commit: adicionando os arquivos

---
+++ src/JS/script.js
@@ -0,0 +1,73 @@
+const produtos = [
+    ['Notebook Dell', 'Notebook Dell I7 14" 500GB 16RAM', 'Notebook', '7600.99', './assets/imgs/notebook dell 1.jpg'],
+    ['Notebook Acer', 'Notebook Acer I5 11" 256GB 16RAM', 'Notebook', '3500.00', './assets/imgs/notebook acer 1.jpg'],
+    ['Notebook Asus', 'Notebook Asus I9 14" 1TB 32RAM', 'Notebook', '9800.00', './assets/imgs/notebook asus 1.jpg'],
+    ['Celular Iphone', 'Celular Iphone 17 500GB 8RAM', 'Celular', '7300.59', './assets/imgs/iphone 1.png'],
+    ['Playstation 5', 'PS5 Slim Wolverine Edition', 'Videogames', '4300', './assets/imgs/ps5 1.webp'],
+    ['PC Gamer Completo', 'PC Gamer Ryzen 7 7600', 'PC', '6400', './assets/imgs/pc gamer 1.webp'],
+    ['Nintendo Switch Oled', 'Nintendo Switch', 'Videogames', '3200', './assets/imgs/nintendo 1.jpg'],
+    ['Mouse Gamer Logitech', 'Mouse Gamer Logitech', 'Periféricos', '220', './assets/imgs/mouse gamer 1.avif'],
+    ['PS Portal', 'PS Portal PS5', 'Videogames', '1550', './assets/imgs/ps portal 1.jpg'],
+    ['Fingerboard 38mm', 'Skate de dedo', 'Brinquedos', '120', './assets/imgs/skate de dedo 1.jpg'],
+    ['Xbox Series S', 'Xbox Series S Fortnite Edition','Videogames', '2800', './assets/imgs/xbox series s 1.webp'],
+    ['Xbox Series X', 'Xbox Series X Preto', 'Videogames', '4800', './assets/imgs/xbox series x.jpg']
+];
+const sectionCard = document.querySelector(".cards");
+const pesquisa = document.getElementById('pesquisa');
+
+function criarCard(categoria){
+    sectionCard.innerHTML = '';
+
+    categoria.forEach((produto) => {
+        const divCard = document.createElement("div");
+        divCard.classList.add("card");
+
+        const tituloCard = document.createElement("h2");
+        tituloCard.classList.add("titulo");
+
+        const textCard = document.createElement("p");
+        const textPreco = document.createElement("h3");
+
+        const categoriaCard = document.createElement('p');
+        categoriaCard.textContent = produto[2];
+
+        const img = document.createElement("img");
+        img.classList.add("img");
+        img.src = produto[4];
+
+        textPreco.classList.add("preco");
+        textCard.textContent = produto[1];
+        tituloCard.textContent = produto[0];
+        textPreco.textContent = `R$${produto[3]}`;
+
+        divCard.appendChild(tituloCard);
+        divCard.appendChild(textCard);
+        divCard.appendChild(categoriaCard)
+        divCard.appendChild(img);
+        divCard.appendChild(textPreco);
+        sectionCard.appendChild(divCard);
+    });
+}
+
+function procurarItem(e){
+    e.preventDefault();
+    const itemProcurado = pesquisa.value.trim();
+
+    let categoria = produtos.filter((produto) => {
+        const colunasParaPesquisar = [produto[0], produto[1], produto[2], produto[3]];
+        return colunasParaPesquisar.some(coluna => (coluna.toLowerCase().includes(itemProcurado.toLowerCase())))
+    })
+
+    if(categoria.length == 0){
+        sectionCard.innerHTML = '';
+        const textoSemItens = document.createElement("p");
+        textoSemItens.textContent = "Item não encontrado :(";
+        textoSemItens.style = "color: red; font-size: 2rem;";
+        sectionCard.appendChild(textoSemItens);
+    } else {
+        criarCard(categoria);
+    }
+}
+
+criarCard(produtos);
+pesquisa.addEventListener('input', procurarItem);
